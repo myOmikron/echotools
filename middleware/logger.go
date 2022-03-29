2f71a49adf62f788f192ec0ed32e7e44320f0b95
myOmikron <git@omikron.dev>
2022-03-29 22:50:19 +0200

    Added logging middleware

---
+++ middleware/logger.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/myOmikron/echotools/logging"
+	"time"
+)
+
+func Logging(log logging.Logger) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			start := time.Now()
+			if err := next(c); err != nil {
+				switch err.(type) {
+				case *echo.HTTPError:
+					httpError := err.(*echo.HTTPError)
+					log.Infof(
+						"%d %s %s %v - %s %s",
+						httpError.Code, c.Request().Method, c.Request().RequestURI, time.Now().Sub(start),
+						c.RealIP(), c.Request().Header.Get("User-Agent"),
+					)
+				default:
+					log.Error(err.Error())
+				}
+			} else {
+				log.Infof(
+					"%d %s %s %v - %s %s",
+					c.Response().Status, c.Request().Method, c.Request().RequestURI, time.Now().Sub(start),
+					c.RealIP(), c.Request().Header.Get("User-Agent"),
+				)
+			}
+			return nil
+		}
+	}
+}
